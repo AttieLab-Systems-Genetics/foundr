f3dd0c4b90dcb5f275130b76521a9fe505fa72f7
Brian Yandell <byandell@wisc.edu>
2023-01-20 17:12:54 -0600

    fix scatplot for sex, strain, condition

--- R/scatplot.R
+++ R/scatplot.R
@@ -15,12 +15,19 @@ scatplot <- function(data, x, y) {
   data <- 
     tidyr::pivot_wider(data, names_from = "trait", values_from = "value")
   
-  ggplot2::ggplot(data) +
-    ggplot2::aes(.data[[x]], .data[[y]], col = Condition) +
+  p <- ggplot2::ggplot(data) +
+    ggplot2::aes(.data[[x]], .data[[y]]) +
     ggplot2::geom_point() +
     ggplot2::geom_smooth(method = "lm", se = FALSE, formula = y ~ x) +
-    ggplot2::facet_grid(Sex ~ Strain) +
+    ggplot2::facet_grid(sex ~ strain) +
     ggplot2::scale_x_log10() +
-    ggplot2::scale_y_log10() +
-    ggplot2::ggtitle(paste(x, "vs", y, "by Condition"))
+    ggplot2::scale_y_log10()
+  
+  if(condition %in% names(data)) {
+    p <- p + ggplot::aes(col = condition) +
+      ggplot2::ggtitle(paste(x, "vs", y, "by condition"))
+  } else {
+    p <- p + ggplot2::ggtitle(paste(x, "vs", y))
+  }
+  p
 }
