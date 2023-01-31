25e9709f2147f7dbd8ed1d1c266a0b835d50ceda
Brian Yandell <byandell@wisc.edu>
2023-01-30 18:26:20 -0600

    add pivot_pair routine

---
+++ R/pivot_pair.R
@@ -0,0 +1,22 @@
+#' Pivot pair of traits
+#'
+#' @param object data frame with specific columns
+#' @param pair 2-vector of trait names
+#'
+#' @return data frame
+#' @export
+#' @importFrom dplyr filter
+#' @importFrom tidyr pivot_wider
+#'
+#' @examples
+pivot_pair <- function(object, pair) {
+  dplyr::filter(
+    tidyr::pivot_wider(
+      dplyr::filter(
+        object,
+        trait %in% c(pair[1],pair[2])),
+      names_from = "trait", values_from = "value"),
+    # Make sure x and y columns have no missing data.
+    !(is.na(.data[[pair[1]]]) | is.na(.data[[pair[2]]])))
+  
+}
