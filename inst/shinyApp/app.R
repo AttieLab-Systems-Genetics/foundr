6ac2460bb628a6b9836cef825c83ddd86914ec07
Brian Yandell <byandell@wisc.edu>
2023-03-03 09:39:32 -0600

    add cowplot to internal app.R

--- inst/shinyApp/app.R
+++ inst/shinyApp/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 devtools::install_cran("plotly") # plotly not yet on UW dataviz
 devtools::install_cran("markdown") # markdown not yet on UW dataviz
+devtools::install_cran("cowplot") # patchwork not yet on UW dataviz
 devtools::install_cran("patchwork") # patchwork not yet on UW dataviz
 #devtools::install_github("byandell/foundr")
 library(foundr)
