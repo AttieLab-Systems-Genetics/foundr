bfad4aadffbf7b9bc5a34ad52f43a7d93d3743d7
Brian Yandell <byandell@wisc.edu>
2023-03-29 16:38:08 -0500

    0.7.4 add area_under_curve from zoo

---
+++ R/area_under_curve.R
@@ -0,0 +1,31 @@
+#' Calculate area under curve over minutes
+#'
+#' @param object data frame with `value` and other columns
+#'
+#' @return object collapsed over minutes with AUC
+#' @export
+#' @importFrom dplyr arrange group_by mutate n summarize ungroup
+#' @importFrom rlang .data
+#' @importFrom zoo rollmean
+#'
+#' @examples
+area_under_curve <- function(object) {
+  cols <- c("strain", "sex", "condition", "animal", "week", "trait")
+  cols <- cols[cols %in% names(object)]
+  
+  dplyr::mutate(
+    dplyr::ungroup(
+      dplyr::summarize(
+        dplyr::arrange(
+          dplyr::group_by(
+            dplyr::mutate(
+              object,
+              minutes = as.numeric(.data$minutes)),
+            dplyr::across(dplyr::all_of(cols))),
+          .data$minutes),
+        value = sum(
+          diff(.data$minutes) * 
+            zoo::rollmean(.data$value, 2)) / 60,
+        .groups = "drop")),
+    trait = paste0(.data$trait, "_AUC"))
+}
