39efb9032f40a855652995abc06a9f55ae21edad
byandell <byandell@DOIT-RCI-006009.wisc.edu>
2023-08-22 15:45:44 -0500

    0.9.23 modify harmonize to make sure there are enough strains

--- R/harmonize.R
+++ R/harmonize.R
@@ -6,6 +6,7 @@
 #' @param ... additional optional parameters to `userHarmony()`
 #' @param normalize apply `normalscores` if `TRUE`
 #' @param condition_name name of `condition` column if present.
+#' @param minstrains minimum number of `strains` allowed per `trait`
 #'
 #' @return side action to save RDS files locally
 #' @export
@@ -14,7 +15,8 @@
 #'
 harmonize <- function(dataset, links, userHarmony, ...,
                       normalize = TRUE,
-                      condition_name = "condition") {
+                      condition_name = "condition",
+                      minstrains = nstrains - 2) {
   # Harmonize data with user-supplied harmony function.
   # Function must have `dataset` as first argument and include `...` argument.
   cat("Harmonizing raw data ...\n", stderr())
@@ -44,11 +46,37 @@ harmonize <- function(dataset, links, userHarmony, ...,
     dplyr::filter(
       traitStats,
       !(.data$trait %in% dropTraits))
-  # Additional traits were dropped due to failed fit. Keep what is left.
+  # Additional traits were dropped due to failed fit.
   keepTraits <- unique(traitStats$trait)
   
+  # Drop traits that are missing more than one strain.
+  # First reduce traitData to traits being kept.
+  traitData <-
+    dplyr::filter(
+      traitData,
+      .data$trait %in% keepTraits)
+  
+  # Identify traits with at least minstrains strains.
+  keepTraits <-
+    dplyr::count(
+      dplyr::distinct(
+        traitData, 
+        .data$trait, .data$strain),
+      .data$trait, name = "strains")
+  nstrains <- max(keepTraits$strains) # for default value of minstrains
+  keepTraits  
+    dplyr::filter(
+      keepTraits,
+      .data$strains >= minstrains)$trait
+  
+  # Create `dataset` directory if not there.
   if(!dir.exists(dataset))
     dir.create(dataset)
+  
+  traitStats <- 
+    dplyr::filter(
+      traitStats,
+      .data$trait %in% keepTraits)
   
   saveRDS(traitStats, file.path(dataset, paste0(dataset, "Stats.rds")))
   
